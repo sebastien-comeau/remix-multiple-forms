3abb3d94c8af3c41ce849bfa469ad61405fbab2f
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2024-07-03 09:46:07 -0300

    Setup multiple-forms example

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,48 +1,97 @@
-import type { MetaFunction } from "@remix-run/node";
+import {
+  ActionFunctionArgs,
+  json,
+  redirect,
+  type MetaFunction,
+} from '@remix-run/node';
+import { Form, useLoaderData } from '@remix-run/react';
+import {
+  deleteInvitation,
+  getInvitations,
+  resendInvitation,
+  sendInvitation,
+} from '~/data.server';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: 'New Remix App' },
+    { name: 'description', content: 'Welcome to Remix!' },
   ];
 };
 
+export const loader = async () => {
+  return json({ invitations: await getInvitations() });
+};
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const formData = await request.formData();
+  if (formData.get('intent') === 'send') {
+    const email = formData.get('email');
+    if (typeof email !== 'string') {
+      // you'll want to handle this in a real app...
+      throw new Error('make sure you implement validation');
+    }
+
+    await sendInvitation(email);
+    return redirect(request.url);
+  }
+
+  const invitationId = formData.get('invitationId');
+  if (!invitationId) {
+    // you'll want to handle this in a real app...
+    throw new Error('make sure you implement validation');
+  }
+  const invitations = await getInvitations();
+  const invitation = invitations.find((i) => i.id === invitationId);
+  if (!invitation) {
+    // you'll want to handle this in a real app...
+    throw new Error('make sure you implement validation');
+  }
+
+  if (formData.get('intent') === 'resend') {
+    await resendInvitation(invitation);
+    return redirect(request.url);
+  }
+  if (formData.get('intent') === 'delete') {
+    await deleteInvitation(invitation);
+    return redirect(request.url);
+  }
+};
+
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
+
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
+    <div>
+      <h1>Invitations:</h1>
+      <ul>
+        {data.invitations.map((invitation) => (
+          <li key={invitation.id}>
+            <Form method="post">
+              <input type="hidden" name="invitationId" value={invitation.id} />
+              {`${invitation.email} last sent ${new Date(
+                invitation.sentTime
+              ).toLocaleTimeString()}: `}
+              <button type="submit" name="intent" value="resend">
+                Resend
+              </button>
+              <button type="submit" name="intent" value="delete">
+                Delete
+              </button>
+            </Form>
+          </li>
+        ))}
       </ul>
+      <Form method="post">
+        Send a new invitation:
+        <br />
+        <label>
+          Email: <input type="email" name="email" />
+        </label>
+        <button type="submit" name="intent" value="send">
+          Send
+        </button>
+      </Form>
     </div>
   );
 }
